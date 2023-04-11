353a5cef3758ece4e3094e6d5ee1d0bfd59b46be
jack <jack.chenyuana@gmail.com>
2023-04-11 19:49:49 +0800

    :construction: wip: 新增网站描述和检索关键词

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,11 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>前端森林</title>
         <meta
           name="keywords"
-          content="前端，"
+          content="前端,学习,搜索引擎,导航,前端森林,前端工具,前端工具箱,网站收集,创作,ai,开源项目,官方文档,免费接口,前端酷站,前端资源,计算机,计算机学习,前端导航,前端工具集,前端工具大全,前端工具库,前端工具推荐,前端框架推荐"
         />
         <meta
           name="description"
-          content="前端酷站 尽收眼底"
+          content="收录前端各类技术站点，包括但不限于前端技术站点、技术周刊、高质量个人博客、前端公众号、技术规范、开源项目站点等。提供前端开发中常用的工具和资源，如图标库、正则表达式、CSS、JavaScript、服务等。"
         />
       </Head>
       <AppProvider>
