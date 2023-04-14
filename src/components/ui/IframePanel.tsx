6b414ee1bd9bb269cae39dbc7901aab3d6191b14
jack <jack.chenyuana@gmail.com>
2023-04-14 19:22:36 +0800

    :bug: fix: ai加载动画

--- src/components/ui/IframePanel.tsx
+++ src/components/ui/IframePanel.tsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   url: string
   [key: string]: any
 }
 
+const MINIMUM_DISPLAY_TIME = 1000
+
 const IframePanel = ({ url, ...props }: Props) => {
   const [loading, setLoading] = useState(true)
+  const [startTime, setStartTime] = useState(Date.now())
+
+  function delayHideLoading() {
+    let elapsedTime = Date.now() - startTime
+    if (elapsedTime < MINIMUM_DISPLAY_TIME) {
+      setTimeout(() => {
+        setLoading(false)
+      }, MINIMUM_DISPLAY_TIME - elapsedTime)
+    } else {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    setLoading(() => {
+      setStartTime(Date.now())
+      return true
+    })
+  }, [url])
 
   return (
     <>
@@ -14,7 +35,7 @@ const IframePanel = ({ url, ...props }: Props) => {
         <iframe
           className={`w-full h-full ${loading ? 'invisible' : 'visible'}`}
           src={url}
-          onLoad={() => setLoading(false)}
+          onLoad={delayHideLoading}
         />
         {loading && (
           <div className="w-full h-full absolute top-0 left-0 text-5xl text-center leading-[90vh]">
