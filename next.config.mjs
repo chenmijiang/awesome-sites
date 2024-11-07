5468b01bf768d10e6fdadb2cf0e2ebf6d83b6828
chenhuaiyuan <chenhuaiyuan@algorix.co>
2024-11-07 21:17:07 +0800

    wip: temp my work

---
+++ next.config.mjs
@@ -0,0 +1,44 @@
+import createMDX from "@next/mdx";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cn.bing.com",
+        port: "",
+        pathname: "/th",
+      },
+    ],
+  },
+};
+
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
